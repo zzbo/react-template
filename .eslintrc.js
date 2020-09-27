c70c61d1d21b673ef85c732b78719f0ea40f3254
zzbozheng <zzbozheng@tencent.com>
2020-09-27 23:55:27 +0800

    refactor: 完善 eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,6 @@ module.exports = {
   extends: 'eslint-config-imweb',
   plugins: ['@typescript-eslint', 'react', 'react-hooks'],
   parser: '@typescript-eslint/parser',
-
   parserOptions: {
     project: './tsconfig.json',
   },
@@ -16,5 +15,26 @@ module.exports = {
     "@typescript-eslint/no-use-before-define": ["error"],
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
-  }
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        "js": "never",
+        "jsx": "never",
+        "ts": "never",
+        "tsx": "never"
+      }
+   ]
+  },
+  settings: {
+    "import/extensions": [".js",".jsx",".ts",".tsx"],
+    "import/parsers": {
+        "@typescript-eslint/parser": [".ts",".tsx"]
+    },
+    "import/resolver": {
+      "node": {
+        "extensions": [".js",".jsx",".ts",".tsx"]
+      }
+    }
+  },
 };
